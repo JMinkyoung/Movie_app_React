11f0324bf4ad66ae0589d8859f9cb562cb759745
JMinkyoung <37430920+JMinkyoung@users.noreply.github.com>
2021-02-16 15:55:34 +0900

    CSS 완료

--- movie-app/src/Movie.js
+++ movie-app/src/Movie.js
@@ -7,11 +7,10 @@ function Movie({ year, title, summary, poster, genres}){
         <div className="movie">
             <img src={poster} alt={title} title={title}></img>
             <div className="movie__data">
-                <h3 className="movie__tite">{title}</h3>
+                <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
-                <p className="movie__summary">{summary}</p>
-                    
+                <ul className="movie__genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
+                <p className="movie__summary">{summary.slice(0,180)}...</p>
             </div>
         </div>
     )
